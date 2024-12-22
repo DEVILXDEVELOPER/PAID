1ca2b293b12873d15ede213b61e32c4d95dadce5
DEVILXDEVELOPER <hhhhhhh490000@gmail.com>
2024-12-22 07:25:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-# Your Telegram Bot Token
-BOT_TOKEN = "7770505642:AAHQ_cmnQg8H6H5-9K_97H9DyvIvvjGxh8w"  # Replace with your actual bot token
-
-# Admin User ID
-ADMIN_ID = 6942423757  # Replace with the actual Telegram ID of the admin
